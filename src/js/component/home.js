803beea57e3cda786431071cf6b5b98a167f305c
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-09-12 20:14:49 +0000

    initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,26 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export class Home extends React.Component {
-	render() {
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+export const Home = props => {
+	return (
+		<React.Fragment>
+			<div className="alert alert-primary" role="alert">
+				{props.circus}
 			</div>
-		);
-	}
-}
+		</React.Fragment>
+	);
+};
+
+Home.propTypes = {
+	circus: PropTypes.number
+};
